8567e0907ce4b7736cfcbd4ebd4f67fc1fc6abbc
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-08-20 18:37:14 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(numbers: IntArray, n: Int): Int {
+        var sum = 0
+        for (number in numbers) {
+            sum += number
+            if (sum > n) {
+                return sum
+            }
+        }
+        return sum
+    }
+}
