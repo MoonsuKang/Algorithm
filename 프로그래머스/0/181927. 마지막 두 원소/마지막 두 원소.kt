065f1eb2698b8cb7dd1751af5fd15deca27fd773
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-08-27 17:29:42 +0900

    [level 0] Title: 마지막 두 원소, Time: 13.35 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        val last = num_list[num_list.size -1]
+        val secondLast = num_list[num_list.size -2]
+        
+        val newValue = if (last > secondLast) {
+            last - secondLast
+        } else {
+            last * 2
+        }
+        
+        return num_list + newValue
+    }
+}
