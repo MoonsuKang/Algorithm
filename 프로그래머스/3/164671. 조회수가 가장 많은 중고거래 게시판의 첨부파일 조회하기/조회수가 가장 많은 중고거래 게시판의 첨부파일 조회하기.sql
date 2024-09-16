7f86703c98391fa25dc65fcbf3e76b6c4d1f2f8a
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-09-17 01:31:41 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 입력하세요
+SELECT
+CONCAT('/home/grep/src/',
+       B.BOARD_ID, '/',
+       F.FILE_ID,
+       F.FILE_NAME,
+       F.FILE_EXT
+      ) AS FILE_PATH
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_FILE F ON F.BOARD_ID = B.BOARD_ID
+WHERE B.VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD)
+ORDER BY F.FILE_ID DESC;
+
+
+#FILE_PATH
+#조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회
