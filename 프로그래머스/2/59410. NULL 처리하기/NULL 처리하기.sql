749e9a6b4506bc73a1b430e92c7075af86af876b
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-09-19 10:26:50 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_TYPE, COALESCE(NAME,'No name') AS NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC;
+
+#동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회
+#ANIMAL_TYPE	NAME	SEX_UPON_INTAKE
