0070cd27c682d70b57a7d29dbae3e9761e48ba6d
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-09-20 17:55:23 +0900

    [level 2] Title: 타겟 넘버, Time: 6.23 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 5.23 ms
+메모리: 74.3 MB, 시간: 6.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 14일 13:51:53
+2024년 09월 20일 17:55:19
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,21 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        // System.out.println(numbers.length);
         return dfs(numbers, target, 0, 0);
     }
-    private int dfs(int[] numbers, int target, int index, int sum) {
-        // 모든 숫자를 사용한 경우
-        if(index == numbers.length) {
-            //합이 타겟이랑 같은 경우
+    // index는 현재 탐색 중인 숫자의 위치, sum은 현재까지 계산한 값
+    private int dfs(int[] numbers, int target, int index, int sum){
+        if (index == numbers.length) {
+            // 모든 숫자를 다 사용하면
             if(sum == target) {
-                return 1;
+                return 1; // 타겟 넘버와 같으면 1 증가
             } else {
                 return 0;
             }
         }
-         // 현재 숫자를 더하는 경우와 빼는 경우로 분기처리
-        int count = 0;
-        count += dfs(numbers, target, index+1, sum+numbers[index]); //더하기
-        count += dfs(numbers, target, index+1, sum-numbers[index]); //빼기
         
-        return count;
+        // 현재 숫자를 더하는 경우와 빼는 경우로 재귀적으로 호출
+        return dfs(numbers, target, index+1, sum+numbers[index]) +
+            dfs(numbers, target, index+1, sum-numbers[index]);
+        
     }
 }
-
-// DFS로 현재 숫자를 더하거나 뺀 다음, 다음 숫자로 이동하는 방식으로 구현
-
