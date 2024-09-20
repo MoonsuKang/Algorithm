e316e2c9a43bdee0138c6bc77bf5098b8ed1a18e
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-09-20 18:11:38 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,30 @@
 class Solution {
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n]; // 방문여부 저장
-        int nodeCount = 0;
-            
-        // 모든 컴퓨터 순회
-        for(int i=0; i < n; i++) {
-            // 아직 방문하지 않은 컴퓨터가 있으면 새로운 네트워크로 간주
+        int answer = 0;
+        boolean[] visited = new boolean[n]; // 컴퓨터 방문 여부
+        int networkCount = 0; // 네트워크 개수
+        
+        //모든 컴퓨터를 순회해
+        for(int i=0; i <n; i++){
+            // 아직 방문하지 않은 컴퓨터가 있다면 새로운 네트워크로 간주하고 DFS 수행
             if(!visited[i]) {
                 dfs(computers, visited, i);
-                nodeCount++;
+                networkCount ++;
             }
         }
-        return nodeCount;
+        
+        return networkCount;
     }
     
-    private void dfs(int[][] computers, boolean[] visited, int node) {
-        //현재 노드를 방문 처리해
-        visited[node] = true;
-        // 현재 노드에 연결된 모든 노드 탐색을 해
-        for(int i=0; i < computers.length; i++){
-            // 연결되어 있고 방문 안한노드 dfs
+    private void dfs(int[][] computers, boolean[] visited, int node){
+        visited[node] = true; // 현재 노드 방문처리
+        
+        // 현재 노드에 연결된 모든 노드 탐색
+        for(int i=0;i<computers.length; i++){
+            // 연결되어 있지 않고 방문하지 않은 노드에 대해서 재귀적으로 dfs
             if(computers[node][i] == 1 && !visited[i]) {
                 dfs(computers, visited, i);
             }
         }
     }
 }
-
-// 방문 추적 배열 변수
-// 방문여부 카운트 변수
-// dfs함수 0부터 n-1까지 순회
-// 
