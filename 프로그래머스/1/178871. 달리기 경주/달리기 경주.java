964534078fab0820d6dcd087d4883cd3f6d89100
Moonsu Kang <85223787+MoonsuKang@users.noreply.github.com>
2024-10-10 14:22:13 +0900

    [level 1] Title: 달리기 경주, Time: 673.37 ms, Memory: 375 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> playerMap = new HashMap<>();
+        
+        for(int i=0; i < players.length; i++){
+            playerMap.put(players[i], i); // 인덱스가 키 벨류가 이름
+        }
+        
+        // 추월을 처리해
+        for(String calling: callings) {
+            //추월한 선수 순위
+            int currentIndex = playerMap.get(calling);
+            // 자기 앞에 선수를 추월
+            int prevIndex = currentIndex - 1;
+            
+            String prevPlayer = players[prevIndex]; // 앞에 있던 선수
+            players[prevIndex] = calling; // 추월한 선수가 앞자리로
+            players[currentIndex] = prevPlayer; // 앞에 있던 선수는 한 칸 뒤로
+            
+            // 선수 위치를 바꿔
+            playerMap.put(calling, prevIndex);
+            playerMap.put(prevPlayer, currentIndex);
+            
+        }
+        
+        return players;
+    }
+}
